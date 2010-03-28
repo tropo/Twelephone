682543613c088eea8d0c98db6c4ccba4014f0339
Topher <topher@topher.local>
2010-03-28 10:38:43 -0700

    added twitterid5

--- config/initializers/scheduled_tasks.rb
+++ config/initializers/scheduled_tasks.rb
@@ -57,49 +57,49 @@
 
 
 
-require 'rubygems'
-require 'rest_client'
-require 'tweetstream'
-
-Thread.new do
-TweetStream::Client.new('twelephoneapp','dvtdvt').track('#twelephone') do |status|
-  # puts "#{status.text}"
-  # puts "#{status.id}"
-  # puts "#{status.user.screen_name}"
-  
-  # calls ||= Call.find(:first, :conditions => ['twitterid = ?', status.id])
-  # 
-  # if calls.nil?
-    target = status.text.scan(/@([A-Za-z0-9_]+)/)
-    
-    # logit = Call.new
-    # logit.twitterid = status.id
-    # logit.author = status.user.screen_name
-    # logit.target = target[0]
-    # logit.save
-    
-    source = User.find(:first, :conditions => ['login = ?', status.user.screen_name]) rescue false
-    destination = User.find(:first, :conditions => ['login = ?', target[0]]) rescue false
-    
-    if source and destination
-        dial = RestClient.get URI.encode('http://teleku.com/connect/' + source.phone + '/' + destination.phone ) rescue '' 
-    
-    elsif source.nil?
-      
-      if status.user.screen_name != 'twelephoneapp'
-        resource = RestClient::Resource.new 'http://twitter.com/statuses/update.xml', :user => 'twelephoneapp', :password => 'dvtdvt'
-        resource.post :status => '@' + status.user.screen_name + ': Before giving someone a twelephone call, you must register at http://twelephone.com!', :content_type => 'application/xml'
-      end
-      
-    elsif destination.nil?
-      
-      resource = RestClient::Resource.new 'http://twitter.com/statuses/update.xml', :user => 'twelephoneapp', :password => 'dvtdvt'
-      resource.post :status => '@' + target[0].to_s + ': @' + status.user.screen_name + ' is trying to give you a twelephone call. Register at http://twelephone.com!', :content_type => 'application/xml'
-      
-      
-    end
-    
-  # end
-end
-  
-end
+# require 'rubygems'
+# require 'rest_client'
+# require 'tweetstream'
+# 
+# Thread.new do
+# TweetStream::Client.new('twelephoneapp','dvtdvt').track('#twelephone') do |status|
+#   # puts "#{status.text}"
+#   # puts "#{status.id}"
+#   # puts "#{status.user.screen_name}"
+#   
+#   # calls ||= Call.find(:first, :conditions => ['twitterid = ?', status.id])
+#   # 
+#   # if calls.nil?
+#     target = status.text.scan(/@([A-Za-z0-9_]+)/)
+#     
+#     # logit = Call.new
+#     # logit.twitterid = status.id
+#     # logit.author = status.user.screen_name
+#     # logit.target = target[0]
+#     # logit.save
+#     
+#     source = User.find(:first, :conditions => ['login = ?', status.user.screen_name]) rescue false
+#     destination = User.find(:first, :conditions => ['login = ?', target[0]]) rescue false
+#     
+#     if source and destination
+#         dial = RestClient.get URI.encode('http://teleku.com/connect/' + source.phone + '/' + destination.phone ) rescue '' 
+#     
+#     elsif source.nil?
+#       
+#       if status.user.screen_name != 'twelephoneapp'
+#         resource = RestClient::Resource.new 'http://twitter.com/statuses/update.xml', :user => 'twelephoneapp', :password => 'dvtdvt'
+#         resource.post :status => '@' + status.user.screen_name + ': Before giving someone a twelephone call, you must register at http://twelephone.com!', :content_type => 'application/xml'
+#       end
+#       
+#     elsif destination.nil?
+#       
+#       resource = RestClient::Resource.new 'http://twitter.com/statuses/update.xml', :user => 'twelephoneapp', :password => 'dvtdvt'
+#       resource.post :status => '@' + target[0].to_s + ': @' + status.user.screen_name + ' is trying to give you a twelephone call. Register at http://twelephone.com!', :content_type => 'application/xml'
+#       
+#       
+#     end
+#     
+#   # end
+# end
+#   
+# end
